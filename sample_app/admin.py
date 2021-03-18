ed05075b2b3d1c27b0945b39ee69065c76528c06
shahbaz-educative <79093482+shahbaz-educative@users.noreply.github.com>
2021-03-19 02:23:35 +0500

    lesson 03: list_display property

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from sample_app.models import *
 
-# Register your models here.
-admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name','createdDate','updatedDate',)
+
 admin.site.register(Question)
 admin.site.register(Choice)
