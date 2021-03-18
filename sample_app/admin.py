f45b5a74a56fb0bff8bf1ce06df501f69981be74
shahbaz-educative <79093482+shahbaz-educative@users.noreply.github.com>
2021-03-19 01:59:38 +0500

    lesson 02: creating models and populating fake data

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sample_app.models import *
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Question)
+admin.site.register(Choice)
