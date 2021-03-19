5aaa52a26e723e75bf66531dd20e4b450c84a95b
shahbaz-educative <79093482+shahbaz-educative@users.noreply.github.com>
2021-03-19 15:22:54 +0500

    lesson 04: fieldsets, list_display, list_editable, list_display_links and ordering

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
 from sample_app.models import *
 
+# admin.site.empty_value_display = '(No value)'
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+    
+    empty_value_display = 'Unknown'
     list_display = ('name','createdDate','updatedDate',)
 
-admin.site.register(Question)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+
+    save_on_top = True
+
+    fieldsets = (
+        ("Question information", {'fields': ('question_text',)}),
+        ("Date", {'fields': ('pub_date',)}),
+        ('The author', {'classes': ('collapse',),'fields': ('refAuthor',),}),
+    )
+
+    list_display = ('question_text', 'refAuthor','pub_date','createdDate', 'updatedDate',)
+    list_display_links = ('refAuthor',)
+    list_editable = ('question_text',)
+
+    ordering = ('-pub_date', 'createdDate',)
+
+    date_hierarchy = 'pub_date'
+
+
 admin.site.register(Choice)
