ae976663df96d51cf1d5df8ab044feb54007a482
shahbaz-educative <79093482+shahbaz-educative@users.noreply.github.com>
2021-03-29 17:42:18 +0500

    lesson 13: Dynamic Charts

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -7,6 +7,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from django.contrib.admin import AdminSite
+from django.db.models.functions import TruncDay
+from django.db.models import Count
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+
 
 # admin.site.unregister(User)
 # admin.site.unregister(Group)
@@ -60,6 +65,24 @@ class QuestionInline(admin.StackedInline):
 
 # @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new authors per day
+        chart_data = (
+    	    Author.objects.annotate(date=TruncDay("updatedDate"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        print("Json %s"%as_json)
+        extra_context = extra_context or {"chart_data": as_json}
+        # Call the superclass changelist_view to render the page
+        
+        return super().changelist_view(request, extra_context=extra_context)
 
     empty_value_display = 'Unknown'
     list_display = ('name','createdDate','updatedDate',)
